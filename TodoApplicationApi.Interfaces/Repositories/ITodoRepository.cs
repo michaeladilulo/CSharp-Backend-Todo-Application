a73eaa89a6e039dbb7c3ad20b9e703a9b7caba48
michaeladilulo <michaeladilulo@gmail.com>
2023-10-10 21:47:01 -0600

    ITodo Repository Built

---
+++ TodoApplicationApi.Interfaces/Repositories/ITodoRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using TodoApplicationApi.TodoApplicationApi.DataModel.Models;
+
+namespace TodoApplicationApi.TodoApplicationApi.Interfaces.Repositories
+{
+	public interface ITodoRepository
+	{
+
+		public Task<List<Todo>> GetAllTodosAsync();
+
+		public Task<Todo> GetTodoByIdAsync(int id);
+
+		public Task<Todo> CreateTodoAsync(Todo todo);
+
+		public Task<Todo> UpdateTodoAsync(Todo todo);
+
+		public Task<Todo> DeleteTodoByIdAsync(Todo todo);
+
+	}
+}
+
