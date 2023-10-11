efa6a8dcd2d0a17df411e219ab29a98d457e0a5c
michaeladilulo <michaeladilulo@gmail.com>
2023-10-10 21:16:34 -0600

    Adds Base Entity Model

---
+++ TodoApplicationApi.DataModel/Models/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace TodoApplicationApi.TodoApplicationApi.DataModel.Models
+{
+	public abstract class BaseEntity
+	{
+
+		public DateTime CreatedAt { get; set; }
+
+		public DateTime UpdatedAt { get; set; }
+
+	}
+}
+
