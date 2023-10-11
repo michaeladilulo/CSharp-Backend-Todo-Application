e1819654fa9db84a42218b252599c4fa2488b19b
michaeladilulo <michaeladilulo@gmail.com>
2023-10-10 21:26:38 -0600

    Base Contract

---
+++ TodoApplicationApi.Contracts/Base/BaseResponseContract.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace TodoApplicationApi.TodoApplicationApi.Contracts.Base
+{
+	public class BaseResponseContract<T>
+	{
+
+		public bool Success { get; set; }
+
+		public string? Message { get; set; }
+
+		public T? Data { get; set; }
+
+	}
+}
+
