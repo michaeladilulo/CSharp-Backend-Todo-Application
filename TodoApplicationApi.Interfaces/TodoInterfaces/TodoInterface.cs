69a084a3e2eb7023b9f96162b1166ff7bdd989f3
michaeladilulo <michaeladilulo@gmail.com>
2023-10-10 21:44:27 -0600

    Todo Interface Built

---
+++ TodoApplicationApi.Interfaces/TodoInterfaces/TodoInterface.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using TodoApplicationApi.TodoApplicationApi.DataModel.Models;
+
+namespace TodoApplicationApi.TodoApplicationApi.Interfaces.TodoInterfaces
+{
+	public interface TodoInterface
+	{
+
+		public Task<List<Todo>> GetAllTodosAsync();
+
+		public Task<Todo> GetTodoByIdAsync(int id);
+
+		public Task<Todo> CreateTodoAsync(Todo todo);
+
+		public Task<Todo> UpdateTodoAsync(Todo todo);
+
+		public Task DeleteTodoByIdAsync(int id);
+
+	}
+}
+
