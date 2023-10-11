f0488c16643343726b1b8ad7b1be58f143c21929
michaeladilulo <michaeladilulo@gmail.com>
2023-10-10 21:19:15 -0600

    Todo Model Created

---
+++ TodoApplicationApi.DataModel/Models/Todo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TodoApplicationApi.TodoApplicationApi.DataModel.Models
+{
+	public class Todo : BaseEntity
+	{
+
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+		public int Id { get; set; }
+
+		public string Title { get; set; }
+
+	}
+}
+
