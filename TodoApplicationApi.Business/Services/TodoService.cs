330e055f6d3cd2ebeb05d1be32e08e04486cfd0f
michaeladilulo <michaeladilulo@gmail.com>
2023-10-10 22:07:52 -0600

    Service File stubbed out

---
+++ TodoApplicationApi.Business/Services/TodoService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using TodoApplicationApi.TodoApplicationApi.DataModel.Models;
+using TodoApplicationApi.TodoApplicationApi.Exceptions;
+using TodoApplicationApi.TodoApplicationApi.Interfaces.Repositories;
+using TodoApplicationApi.TodoApplicationApi.Interfaces.TodoInterfaces;
+
+namespace TodoApplicationApi.TodoApplicationApi.Business.Services;
+
+public class TodoService : TodoInterface
+{
+	private readonly ITodoRepository _todoRepository;
+
+	public TodoService(ITodoRepository todoRepository)
+	{
+		_todoRepository = todoRepository;
+	}
+
+    public Task<Todo> CreateTodoAsync(Todo todo)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteTodoByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<List<Todo>> GetAllTodosAsync()
+    {
+        List<Todo> todos = await _todoRepository.GetAllTodosAsync() ?? throw new EntityNotFoundException($"Todos Not Found");
+
+        return todos;
+    }
+
+    public Task<Todo> GetTodoByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Todo> UpdateTodoAsync(Todo todo)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ TodoApplicationApi.Service/Controllers/TodoController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using TodoApplicationApi.TodoApplicationApi.Contracts.Base;
+using TodoApplicationApi.TodoApplicationApi.DataModel.Models;
+using TodoApplicationApi.TodoApplicationApi.Exceptions;
+using TodoApplicationApi.TodoApplicationApi.Interfaces.TodoInterfaces;
+
+namespace TodoApplicationApi.TodoApplicationApi.Service.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+
+public class TodoController : ControllerBase
+{
+
+	private readonly TodoInterface _todoInterface;
+
+	public TodoController(TodoInterface todoInterface)
+	{
+		_todoInterface = todoInterface;
+	}
+
+	[HttpGet]
+
+	public async Task<ActionResult<BaseResponseContract<Todo>>> GetAllTodosAsync()
+	{
+		BaseResponseContract<List<Todo>> response = new BaseResponseContract<List<Todo>>();
+
+		try
+		{
+			List<Todo> todos = await _todoInterface.GetAllTodosAsync();
+
+			response.Data = todos;
+			response.Success = true;
+
+			return Ok(response);
+		}
+		catch(EntityNotFoundException e)
+		{
+			response.Message = e.Message;
+			response.Success = false;
+
+			return NotFound(response);
+		}
+		catch(Exception e)
+		{
+			response.Message = e.Message;
+			response.Success = false;
+
+			return StatusCode(StatusCodes.Status500InternalServerError, response);
+		}
+	}
+		
+}
