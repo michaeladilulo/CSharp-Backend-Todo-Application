e5973610a30d9297070ffc97033b57b440ab972c
michaeladilulo <michaeladilulo@gmail.com>
2023-10-10 20:57:48 -0600

    Entity Not Found Exception Class

---
+++ TodoApplicationApi.Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace TodoApplicationApi.TodoApplicationApi.Exceptions
+{
+	public class EntityNotFoundException : Exception
+	{
+
+		public string Message { get; set; }
+
+		public EntityNotFoundException(string message) : base(message)
+		{
+			Message = message;
+		}
+
+	}
+}
+
